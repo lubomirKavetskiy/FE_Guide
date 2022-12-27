ec7fc484f79083cee2d72b661b15c04932bf5fed
Lubomir Kavetskiy <lubomirkavetskiy@gmail.com>
2022-12-27 19:31:38 +0200

    Add images

--- HOME/comparing_perfomance_of_concat_spread_push_unshift.js
+++
@@ -1,3 +0,0 @@
-
-Add an element to the array => https://drive.google.com/file/d/16rWwSeRXhjPRjQHcsnc68wG5VXspUM7M/view
-Add an array1 to the array2 => https://drive.google.com/file/d/1qPMfL33ztDip56S6Z03RKXDIOaaDJ2Mo/view
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FE_Guide
 
+Materials for improving and developing frontend developers
+
 ![Screenshot](header.jpeg)
