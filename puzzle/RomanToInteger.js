0f4e359f02e8f3a2f384b5fda824afed80537647
Lubomir Kavetskiy <lubomirkavetskiy@gmail.com>
2023-06-26 13:11:10 +0300

    Create RomanToInteger.js

---
+++ puzzle/RomanToInteger.js
@@ -0,0 +1,29 @@
+const list = new Map();
+list.set('I', 1).set('V', 5).set('X', 10).set('L', 50).set('C', 100).set('D', 500).set('M', 1000);
+
+
+const romanToInt = (romanStr: string):number => {
+  let res = 0;
+  let currIdx = 0;
+
+  for(const s of romanStr) {
+    const currNumber = list.get(s);
+    const prevRomanStr = romanStr[currIdx - 1];
+
+    res += currNumber;
+   
+    if((s === 'V' || s === 'X') && prevRomanStr === 'I') {
+      res -= 2;
+    }
+    else if((s === 'L' || s === 'C') && prevRomanStr === 'X') {
+      res -= 20;
+    }
+    else if((s === 'D' || s === 'M') && prevRomanStr === 'C') {
+      res -= 200;
+    };
+
+    currIdx++;
+  }
+
+  return res;
+};
